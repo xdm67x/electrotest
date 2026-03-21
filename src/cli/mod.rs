9428d32f7fa9591b44072eb14675a2787b39a4d2
xdm67x <m-ozkan@outlook.fr>
2026-03-21 18:55:26 +0100

    fix: restore task 1 cli spec compliance

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -7,13 +7,7 @@ pub struct Cli {
     command: Option<Commands>,
 }
 
-impl Cli {
-    pub fn command(&self) -> Option<&Commands> {
-        self.command.as_ref()
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Subcommand)]
+#[derive(Subcommand)]
 pub enum Commands {
     Init,
     Doctor,
@@ -21,15 +15,7 @@ pub enum Commands {
     Test,
 }
 
-pub fn parse_args<I, T>(args: I) -> Cli
-where
-    I: IntoIterator<Item = T>,
-    T: Into<std::ffi::OsString> + Clone,
-{
-    Cli::parse_from(args)
-}
-
 pub async fn run() -> Result<(), crate::Error> {
-    let _ = parse_args(std::env::args_os());
+    let _ = Cli::parse();
     Ok(())
 }
--- tests/cli_init.rs
+++ tests/cli_init.rs
@@ -1,19 +1,11 @@
 use assert_cmd::Command;
-use electrotest::cli::{parse_args, Commands};
 
 #[test]
-fn prints_help_for_init_subcommand() {
+fn prints_help_for_top_level_cli() {
     Command::cargo_bin("electrotest")
         .unwrap()
-        .args(["init", "--help"])
+        .arg("--help")
         .assert()
         .success()
-        .stdout(predicates::str::contains("Usage: electrotest init"));
-}
-
-#[test]
-fn parses_init_subcommand() {
-    let cli = parse_args(["electrotest", "init"]);
-
-    assert!(matches!(cli.command(), Some(Commands::Init)));
+        .stdout(predicates::str::contains("electrotest"));
 }
